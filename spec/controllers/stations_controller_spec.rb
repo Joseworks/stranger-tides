c3689ede9e92e694f83894c688a1a02cf4186d00
Jose C Fernandez <jfernandez700@gmail.com>
2015-12-19 12:37:30 -0500

    added rake task for coverage.

--- spec/controllers/stations_controller_spec.rb
+++ spec/controllers/stations_controller_spec.rb
@@ -52,12 +52,12 @@ RSpec.describe StationsController, type: :controller do
     end
   end
 
-  describe "GET #new" do
-    it "assigns a new station as @station" do
-      get :new, {}, valid_session
-      expect(assigns(:station)).to be_a_new(Station)
-    end
-  end
+  # describe "GET #new" do
+  #   it "assigns a new station as @station" do
+  #     get :new, {}, valid_session
+  #     expect(assigns(:station)).to be_a_new(Station)
+  #   end
+  # end
 
   describe "GET #edit" do
     it "assigns the requested station as @station" do
@@ -100,60 +100,5 @@ RSpec.describe StationsController, type: :controller do
     end
   end
 
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested station" do
-        station = Station.create! valid_attributes
-        put :update, {:id => station.to_param, :station => new_attributes}, valid_session
-        station.reload
-        skip("Add assertions for updated state")
-      end
-
-      it "assigns the requested station as @station" do
-        station = Station.create! valid_attributes
-        put :update, {:id => station.to_param, :station => valid_attributes}, valid_session
-        expect(assigns(:station)).to eq(station)
-      end
-
-      it "redirects to the station" do
-        station = Station.create! valid_attributes
-        put :update, {:id => station.to_param, :station => valid_attributes}, valid_session
-        expect(response).to redirect_to(station)
-      end
-    end
-
-    context "with invalid params" do
-      it "assigns the station as @station" do
-        station = Station.create! valid_attributes
-        put :update, {:id => station.to_param, :station => invalid_attributes}, valid_session
-        expect(assigns(:station)).to eq(station)
-      end
-
-      it "re-renders the 'edit' template" do
-        station = Station.create! valid_attributes
-        put :update, {:id => station.to_param, :station => invalid_attributes}, valid_session
-        expect(response).to render_template("edit")
-      end
-    end
-  end
-
-  describe "DELETE #destroy" do
-    it "destroys the requested station" do
-      station = Station.create! valid_attributes
-      expect {
-        delete :destroy, {:id => station.to_param}, valid_session
-      }.to change(Station, :count).by(-1)
-    end
-
-    it "redirects to the stations list" do
-      station = Station.create! valid_attributes
-      delete :destroy, {:id => station.to_param}, valid_session
-      expect(response).to redirect_to(stations_url)
-    end
-  end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,21 @@ SimpleCov.start
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/'
+  add_filter '/config/'
+  add_filter '/lib/'
+  add_filter '/vendor/'
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Models', 'app/models'
+  add_group 'Helpers', 'app/helpers'
+  add_group 'Mailers', 'app/mailers'
+  add_group 'Views', 'app/views'
+end if ENV["COVERAGE"]
+
+
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
