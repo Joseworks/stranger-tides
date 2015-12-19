42ded771d0ad7d7a235ff92e9cb5e4a82e7afc2f
Jose C Fernandez <jfernandez700@gmail.com>
2015-12-19 12:47:22 -0500

    Passing routes test.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'show_station' => 'stations#show_station'
   get 'show_stations' => 'stations#show_stations'
   post 'show_graph' => 'stations#show_graph'
-  root 'stations#show_station'
+  root 'stations#show_stations'
 end
--- spec/routing/stations_routing_spec.rb
+++ spec/routing/stations_routing_spec.rb
@@ -3,37 +3,18 @@ require "rails_helper"
 RSpec.describe StationsController, type: :routing do
   describe "routing" do
 
-    it "routes to #index" do
-      expect(:get => "/stations").to route_to("stations#index")
+    it "routes to #show_stations" do
+      expect(:get => "/show_stations").to route_to("stations#show_stations")
     end
 
-    it "routes to #new" do
-      expect(:get => "/stations/new").to route_to("stations#new")
+    it "routes to #show_station" do
+      expect(:get => "/show_station").to route_to("stations#show_station")
     end
 
-    it "routes to #show" do
-      expect(:get => "/stations/1").to route_to("stations#show", :id => "1")
+    it "routes to #show_graph" do
+      expect(:post => "/show_graph").to route_to("stations#show_graph")
     end
 
-    it "routes to #edit" do
-      expect(:get => "/stations/1/edit").to route_to("stations#edit", :id => "1")
-    end
-
-    it "routes to #create" do
-      expect(:post => "/stations").to route_to("stations#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(:put => "/stations/1").to route_to("stations#update", :id => "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(:patch => "/stations/1").to route_to("stations#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      expect(:delete => "/stations/1").to route_to("stations#destroy", :id => "1")
-    end
 
   end
 end
