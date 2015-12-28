6f13817fa5b73c5356bd01d16bf9195d47643ae1
JoseC <jfernandez700@gmail.com>
2015-12-27 19:53:27 -0500

    Separated rake task stations_id_list, added test.

---
+++ spec/lib/tasks/stations_id_list_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+require 'rake'
+require 'rails_helper'
+
+describe 'stations_id_list' do
+  before(:each) do
+    load File.join(Rails.root, 'lib', 'tasks', 'stations_id_list.rake')
+    Rake::Task.define_task(:environment)
+    Rake::Task["stations_id_list:process_station_list"].invoke
+  end
+
+  describe 'stations_id_list:process_station_list' do
+    it "should add all the stations ids to a station" do
+        expect(Station.last.tide_info).to include("9461710")
+        expect(Station.last.tide_info).not_to be_nil
+    end
+  end
+end
+
+
