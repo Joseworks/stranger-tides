92900677da805b7bf02169b1a5b6a7f634cc1d76
Joseworks <josefernandez@joseworks.org>
2016-04-20 00:22:57 -0400

    Minor edits.

--- app/models/station_data_range_constructor.rb
+++ app/models/station_data_range_constructor.rb
@@ -4,17 +4,19 @@ class StationDataRangeConstructor
 
     attr_accessor :my_station,:product,:begin_date,:begin_time,:end_date,:end_time,:datum,:units,:time_zone,:application,:format,:url, :range_constructor
     def initialize(station_id)
-      @range_constructor ={  my_station:  station_id,
-                            product:  'water_level',
-                            begin_date:  2.days.ago.strftime("%Y%m%d"),
-                            begin_time:  2.days.ago.strftime('%R'),
-                            end_date:  Time.now.strftime("%Y%m%d"),
-                            end_time:  Time.now.strftime('%R'),
-                            datum:  'mllw',
-                            units: 'english',
-                            time_zone: 'gmt',
-                            application: 'web_services',
-                            format: 'json'
-                          }
+      @range_constructor=
+      {
+        my_station:  station_id,
+        product:  'water_level',
+        begin_date:  2.days.ago.strftime("%Y%m%d"),
+        begin_time:  2.days.ago.strftime('%R'),
+        end_date:  Time.now.strftime("%Y%m%d"),
+        end_time:  Time.now.strftime('%R'),
+        datum:  'mllw',
+        units: 'english',
+        time_zone: 'gmt',
+        application: 'web_services',
+        format: 'json'
+      }
     end
 end
--- spec/models/station_data_range_constructor_spec.rb
+++ spec/models/station_data_range_constructor_spec.rb
@@ -6,21 +6,21 @@ RSpec.describe StationDataRangeConstructor, type: :model do
     current_station = StationDataRangeConstructor.new(station_id)
     subject {current_station.range_constructor}
 
-    it { is_expected.to have_key(:my_station)}
-    it { is_expected.to have_key(:product)}
-    it { is_expected.to have_key(:begin_date)}
-    it { is_expected.to have_key(:begin_time)}
-    it { is_expected.to have_key(:end_date)}
-    it { is_expected.to have_key(:end_time)}
-    it { is_expected.to have_key(:datum)}
-    it { is_expected.to have_key(:units)}
-    it { is_expected.to have_key(:time_zone)}
-    it { is_expected.to have_key(:application)}
-    it { is_expected.to have_key(:format)}
-    it { is_expected.to include(my_station: 1234567)}
-    it { is_expected.to include(product: 'water_level')}
-    it { is_expected.to include(format: 'json')}
-  end
+      it { is_expected.to have_key(:my_station)}
+      it { is_expected.to have_key(:product)}
+      it { is_expected.to have_key(:begin_date)}
+      it { is_expected.to have_key(:begin_time)}
+      it { is_expected.to have_key(:end_date)}
+      it { is_expected.to have_key(:end_time)}
+      it { is_expected.to have_key(:datum)}
+      it { is_expected.to have_key(:units)}
+      it { is_expected.to have_key(:time_zone)}
+      it { is_expected.to have_key(:application)}
+      it { is_expected.to have_key(:format)}
+      it { is_expected.to include(my_station: 1234567)}
+      it { is_expected.to include(product: 'water_level')}
+      it { is_expected.to include(format: 'json')}
+    end
 
   context 'with invalid station_id' do
     station_id = nil
