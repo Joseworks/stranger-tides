13038490569925c8a827d75a6a0e575e968e0515
Jose C Fernandez <jfernandez@pwnhealth.org>
2023-03-08 19:23:31 -0500

    Update migrations

--- db/migrate/20151227112425_remove_station_id_from_stations.rb
+++ db/migrate/20151227112425_remove_station_id_from_stations.rb
@@ -1,4 +1,4 @@
-class RemoveStationIdFromStations < ActiveRecord::Migration
+class RemoveStationIdFromStations < ActiveRecord::Migration[4.2]
   def change
     remove_column :stations, :station_id
   end
