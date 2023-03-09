ca33f9aec3e850baeb19f47a694826bfe6de5f86
Jose C Fernandez <jfernandez@pwnhealth.org>
2023-03-08 19:01:23 -0500

    Minor

--- config/puma.rb
+++ config/puma.rb
@@ -5,6 +5,7 @@ threads threads_count, threads_count
 preload_app!
 
 rackup      DefaultRackup if defined?(DefaultRackup)
+
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
