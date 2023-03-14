f0266c0ae0df26ad617e6a1967bcdf4466e4cbe5
Jose C Fernandez <jfernandez@pwnhealth.org>
2023-03-14 15:16:36 -0400

    Update rails initializers

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2015_12_27_112425) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_190912) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "stations", id: :serial, force: :cascade do |t|
     t.string "station_name"
     t.text "tide_info", default: [], array: true
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.json "metadata", default: {}, null: false
   end
 
